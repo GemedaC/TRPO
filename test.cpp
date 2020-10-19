bf6d92de25170b3320e36274a6d864471bc244d8
GemedaC <72394968+GemedaC@users.noreply.github.com>
2020-10-19 11:17:53 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,9 +4,8 @@
 #include <string>
 
 
-int main(int argc, const char* argv[]) {
-	std::string Test = "Test";
-	std::cout << Test << std::endl;
+int main(int argc, char* argv[]) {
+	std::cout << "Hello world" << std::endl;
 	
 	return 0;
 }
