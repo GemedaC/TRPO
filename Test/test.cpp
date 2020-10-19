bbfd3da037796f9e06498a0482de2a49641674a8
Nikita <nkrivorotov@bk.ru>
2020-10-19 09:12:13 +0700

    update test.cpp

---
+++ Test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+
+
+int main(int argc, const char* argv[]) {
+	std::string Test = "Test";
+	std::cout << Test << std::endl;
+	
+	return 0;
+}
