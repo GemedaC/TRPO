0232fccbd753e034f683d5c9f3dd7fabb9ecba9d
Nikita <nkrivorotov@bk.ru>
2020-11-04 17:07:58 +0700

    first test

---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+#include "test.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+int main(int argc, const char* argv[])
+{
+	Hello();
+	std::cin.get();
+	return 0;
+}
---
+++ main/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+void Hello()
+{
+	std::cout << "Hello World!" << std::endl;
+}
